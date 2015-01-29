0c8f2b80754842e9791b8543e425435b974491a3
EmilyJohnson <3mle33@gmail.com>
2015-01-29 14:27:36 -0600

    Got a mostly working model of Level0, step 1

--- rps.rb
+++ rps.rb
@@ -1,1 +1,34 @@
- 
+puts "player 1, please choose a move:"
+player1 = gets.chomp.downcase
+puts "player 2, please choose a move:"
+player2 = gets.chomp.downcase
+
+if player1 && player2 != "rock" || "paper" || "scissors"
+  puts "Error!"
+else 
+  #Player 1 wins
+  if player1 == "rock" && player2 == "scissors"
+    puts "Player 1 wins!"
+  elsif player1 == "paper" && player2 == "rock"
+    puts "Player 1 wins!"
+  elsif player1 == "scissors" && player == "paper"
+    puts "Player 1 wins!"
+  #Player 2 wins
+  elsif player1 == "rock" && player2 == "paper"
+    puts "Player 2 wins!"
+  elsif player1 == "paper" && player2 == "scissors"
+    puts "Player 2 wins!"
+  elsif player1 == "scissors" && player2 == "rock"
+    puts "Player 2 wins!"
+  #No one wins
+  elsif player1 == "rock" && player2 == "rock"
+    puts "Tie..."
+  elsif player1 == "paper" && player2 == "paper"
+    puts "Tie..."
+  elsif player1 == "scissors" && player2 == "scissors"
+    puts "Tie..."
+  end
+end
+  
+  
+#binding.pry
