81019062f16413a3b508a61b04378fa2e18d679f
EmilyJohnson <3mle33@gmail.com>
2015-01-29 19:46:31 -0600

    Got my boolean expressions working -- THANKS ANDREW!

--- rps.rb
+++ rps.rb
@@ -1,8 +1,24 @@
 require 'pry'
 
+# Level 0 is where we get things working at their most basic level.
+#
+# 1.Basic rock-paper-scissors game (no custom objects). Output should be sensibly formatted.
+# Should only accept valid moves. Players only need to play a single round.
+#
+# 2.Player classes + overall driver method. Driver method should create players,
+# tell the players to prompt for moves, and determine the winner. Players should
+# keep track of their names and moves, and should still only accept valid moves.
+# From the user’s perspective, the game should appear to work exactly the same.
+#
+# 3.Players should be able to play a best-of-n match, which will require you to keep
+# track of each player’s score.
+#
+# 4.Go back and provide method documentation for all of your classes and custom-written methods.
+# If you wrote the class (or method), you gotta document it.
+
 class Players
   
-  def players_moves
+  #def players_moves
     players = []
     puts "Player 1, what is your name?"
     player1 = gets.chomp.capitalize
@@ -22,32 +38,37 @@ class Players
     attack2 = gets.chomp.downcase
     attacks << attack2
       
-     if attack1 && attack2 != "rock" || "paper" || "scissors"
+    if attack1 != "rock" && attack1 != "paper" && attack1 != "scissors"
       puts "Error!"
-     end
-      #Player 1 wins
-      if attack1 == "rock" && attack2 == "scissors"
-        puts "Player 1 wins!"
-      elsif attack1 == "paper" && attack2 == "rock"
-        puts "Player 1 wins!"
-      elsif attack1 == "scissors" && attack2 == "paper"
-        puts "Player 1 wins!"
-      #Player 2 wins
-      elsif attack1 == "rock" && attack2 == "paper"
-        puts "Player 2 wins!"
-      elsif attack1 == "paper" && attack2 == "scissors"
-        puts "Player 2 wins!"
-      elsif attack1 == "scissors" && attack2 == "rock"
-        puts "Player 2 wins!"
-      #No one wins
-      elsif attack1 == "rock" && attack2 == "rock"
-        puts "Tie..."
-      elsif attack1 == "paper" && attack2 == "paper"
-        puts "Tie..."
-      elsif attack1 == "scissors" && attack2 == "scissors"
-        puts "Tie..."
-      end
+    elsif attack2 != "rock" && attack2 != "paper" && attack2 != "scissors"
+      puts "Error2!"
+    else
+      puts "correct move choices!"
     end
+    #Player 1 wins
+    if attack1 == "rock" && attack2 == "scissors"
+      puts "Player 1 wins!"
+    elsif attack1 == "paper" && attack2 == "rock"
+      puts "Player 1 wins!"
+    elsif attack1 == "scissors" && attack2 == "paper"
+      puts "Player 1 wins!"
+    #Player 2 wins
+    elsif attack1 == "rock" && attack2 == "paper"
+      puts "Player 2 wins!"
+    elsif attack1 == "paper" && attack2 == "scissors"
+      puts "Player 2 wins!"
+    elsif attack1 == "scissors" && attack2 == "rock"
+      puts "Player 2 wins!"
+    #No one wins
+    elsif attack1 == "rock" && attack2 == "rock"
+      puts "Tie..."
+    elsif attack1 == "paper" && attack2 == "paper"
+      puts "Tie..."
+    elsif attack1 == "scissors" && attack2 == "scissors"
+      puts "Tie..."
+    end
+      #end
 end
   
+
 #binding.pry
