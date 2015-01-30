beccd2d89ed58ea2f67172e283681e076c00bdd6
EmilyJohnson <3mle33@gmail.com>
2015-01-30 13:49:05 -0600

    No significant progress, trying to break my code into methods

--- rps.rb
+++ rps.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require_relative 'player'
 
 # Level 0 is where we get things working at their most basic level.
 #
@@ -19,7 +20,7 @@ require 'pry'
 
 
 
-# Class: Players
+# Class: Driver
 #
 # Right now, it contains a rock paper scissors game, and people who play it!
 # I do realize this is a mess. It's a WIP.
@@ -30,98 +31,91 @@ require 'pry'
 # Public Methods:
 # driver
 
-class Players
+class Driver
+  #figuring out HOW to get my arrays into this hash :s 
+  players_and_moves = {}
+  players = []
+
+  #Getting player info - Separate method later?
+  puts "Player 1, what is your name?"
+  player1 = gets.chomp.capitalize
+  #player1 = Player.new
+  players << player1
+  puts "Player 2 what is your name?"
+  player2 = gets.chomp.capitalize
+  #player2 = Player.new
+  players << player2
+
+  puts "#{player1} and #{player2} are you ready for Rock, Paper, Scissors?!?"
+  puts
   
-  # Public: #diver
-  # It does it ALL right now...
-  #
-  # Parameters:
-  # None yet - None yet.
-  #
-  # Returns:
-  # Returns a winner
-  #
-  # State Changes:
-  # Sets winning_player and losing_player.
+  #trying to create more methods...not working...works if these aren't in method...
+  def score
+    @player1_score = 0
+    @player2_score = 0
+  end
+
+  until (@player1_score - 2) == (@player2_score) || (@player2_score - 2) == (@player1_score)
+    #Getting move info - Separate method later?
+    attacks = []
+    puts "#{player1} what is your move?"
+    attack1 = gets.chomp.downcase
+    attacks << attack1 
+    puts "#{player2} what is your move?"
+    attack2 = gets.chomp.downcase
+    attacks << attack2
+
+    #Player 1 wins
+    if attack1 == "rock" && attack2 == "scissors"
+      @player1_score += 1
+      puts "#{player1} wins the battle!"
+      puts
+    elsif attack1 == "paper" && attack2 == "rock"
+      @player1_score += 1
+      puts "#{player1} wins the battle!"
+      puts
+    elsif attack1 == "scisscors" && attack2 == "paper"
+      @player1_score += 1
+      puts "#{player1} wins! the battle"
+      puts
+    #No winners
+    elsif attack1 == "rock" && attack2 == "rock"
+      puts "No one wins"
+      puts
+    elsif attack1 == "paper" && attack2 == "paper"
+      puts "No one wins"
+      puts
+    elsif attack1 == "scissors" && attack2 == "scissors"
+      puts "No one wins!"
+      puts
+    #Player 2 wins
+    elsif attack1 == "rock" && attack2 == "paper"
+      @player2_score += 1
+      puts "#{player2} wins the battle!"
+      puts
+    elsif attack1 == "paper" && attack2 == "scissors"
+      @player2_score += 1
+      puts "#{player2} wins the battle!"
+      puts
+    elsif attack1 == "scissors the battle!" && attack2 == "rock"
+      @player2_score += 1
+      puts "#{player2} wins!"
+      puts
+    else 
+      puts "Not an option..."
+    end
+  end
   
-  #def driver
-    #figuring out HOW to get my arrays into this hash :s 
-    players_and_moves = {}
-    
-    #Using if, elsif, and else blocks, write a program which allows two users to each input their moves in "Rock, Paper, Scissors" and which determines the winner (or if a tie occurred).
+  #trying to come up with some sort of method to reference the rules.
+  def rules
+  end
 
-    #Getting player info - Separate method later?
-    players = []
-    puts "Player 1, what is your name?"
-    player1 = gets.chomp.capitalize
-    players << player1
-    puts "Player 2 what is your name?"
-    player2 = gets.chomp.capitalize
-    players << player2
+  if (@player1_score - 2) == (@player2_score)
+    puts "#{player1} wins the war!!"
+  else
+    puts "#{player2} wins the war!!"
+  end
 
-    puts "#{player1} and #{player2} are you ready for Rock, Paper, Scissors?!?"
-    puts
-
-    player1_score = 0
-    player2_score = 0
-
-    until (player1_score - 2) == (player2_score) || (player2_score - 2) == (player1_score)
-      #Getting move info - Separate method later?
-      attacks = []
-      puts "#{player1} what is your move?"
-      attack1 = gets.chomp.downcase
-      attacks << attack1 
-      puts "#{player2} what is your move?"
-      attack2 = gets.chomp.downcase
-      attacks << attack2
-
-      #Player 1 wins
-      if attack1 == "rock" && attack2 == "scissors"
-        player1_score += 1
-        puts "#{player1} wins!"
-        puts
-      elsif attack1 == "paper" && attack2 == "rock"
-        player1_score += 1
-        puts "#{player1} wins!"
-        puts
-      elsif attack1 == "scisscors" && attack2 == "paper"
-        player1_score += 1
-        puts "#{player1} wins!"
-        puts
-      #No winners
-      elsif attack1 == "rock" && attack2 == "rock"
-        puts "No one wins"
-        puts
-      elsif attack1 == "paper" && attack2 == "paper"
-        puts "No one wins"
-        puts
-      elsif attack1 == "scissors" && attack2 == "scissors"
-        puts "No one wins!"
-        puts
-      #Player 2 wins
-      elsif attack1 == "rock" && attack2 == "paper"
-        player2_score += 1
-        puts "#{player2} wins"
-        puts
-      elsif attack1 == "paper" && attack2 == "scissors"
-        player2_score += 1
-        puts "#{player2} wins"
-        puts
-      elsif attack1 == "scissors" && attack2 == "rock"
-        player2_score += 1
-        puts "#{player2} wins!"
-        puts
-      else 
-        puts "Not an option..."
-      end
-    end
-
-    if (player1_score - 2) == (player2_score)
-      puts "#{player1} wins the war!!"
-    else
-      puts "#{player2} wins the war!!"
-    end
-  #end
 end
   
 
