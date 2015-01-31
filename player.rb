7b19d413d3d3a7eae00d48aa97ecf497581113c2
EmilyJohnson <3mle33@gmail.com>
2015-01-31 12:39:58 -0600

    added ways to view the players scores.

--- player.rb
+++ player.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Player
   #will eventually hold an array of all the players....
   def initialize(players)
@@ -5,18 +7,27 @@ class Player
     @player1_score = 0
     @player2_score = 0
   end 
+  #attr_accessor :players
+
+  def set_player (player)
+    @players << player
+  end
+
+  def get_player
+    @players
+  end
+
+  def add_score(score)
+    score =+ 1
+  end
+  
+  def player1_show_score
+    @player1_score
+  end
+  
+  def player2_show_score
+    @player2_score
+  end
 end
 
-#attr_accessor :players
-
-def set_player (player)
-  @players = player
-end
-
-def get_player
-  @players
-end
-
-def add_score
-  score =+ 1
-end
+binding.pry
